d84f840b38bfc3e5e3ccd133d7900d4e42fe1fd6
binaya-batas <batas.binaya@gmail.com>
2022-10-12 20:04:03 +0545

    best of 5 function added.

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
-console.log("hello")
+let computerSelection;
+let humanSelection;
+let computerScore;
+let playerScore;
 
-function getHumanChoice() {
-    const humanChoice = prompt("Please choose between: 1)🪨 2)📄 3)✂️");
-    return humanChoice.toLowerCase();
-}
-
-function getComputerChoice() {
-    const arr = ["Rock", "Paper", "Scissors"];
+function getcomputerSelection() {
+    const arr = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * 3);
     
     return arr[random];
 }
 
-console.log(getHumanChoice())
-console.log(getComputerChoice())
+game();
+
+function game() {
+    computerScore = 0;
+    playerScore = 0;
+    
+    for(let i = 0; i<5; i++) {
+        gethumanSelection();
+    }
+
+    console.log(declareWinner())
+
+    function declareWinner() {
+        if (playerScore===computerScore) {
+            alert(playerScore + '-' + computerScore + '\nTie game!');
+            return playerScore + '-' + computerScore + '\nTie game!';
+        } else if (playerScore>computerScore) {
+            alert(playerScore + '-' + computerScore + '\nYou win!!');
+            return playerScore + '-' + computerScore + '\nYou win!!';
+        } else {
+            alert(playerScore + '-' + computerScore + '\nYou lost. Better luck next time!');
+            return playerScore + '-' + computerScore + '\nYou lost. Better luck next time!';
+        }
+    }
+}
+
+function gethumanSelection() {
+    humanSelection = prompt("Please choose between: 1)🪨 2)📄 3)✂️");
+    if ((humanSelection.toLowerCase() == "rock") || (humanSelection.toLowerCase() == "paper") || (humanSelection.toLowerCase() == "scissors")) {
+        console.log("Game on!!!😤");
+        console.log(computerSelection);
+        console.log(playRound(humanSelection,computerSelection));
+    } else {
+        alert("invalid input.");
+        gethumanSelection();
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getcomputerSelection().toLowerCase();
+    playerSelection = humanSelection.toLowerCase();
+    console.log(computerSelection);
+    if (computerSelection==playerSelection) {
+        alert('Tie game!');
+        return 'Tie game\nComputer Score: ' + 
+                computerScore + '\nYour Score: ' + playerScore;
+    } else if ((computerSelection=='rock' && playerSelection=='scissors') 
+            || (computerSelection=='scissors' && playerSelection=='paper') 
+            || (computerSelection=='paper' && playerSelection=='rock')) {
+        alert('You lose! ' + computerSelection + ' beats ' + playerSelection);
+        return 'Computer Score: ' + ++computerScore + 
+                '\nYour Score: ' + playerScore;
+    } else {
+        alert('You win! ' + playerSelection + ' beats ' + computerSelection);
+        return 'Your Score: ' + ++playerScore + 
+                '\nComputer Score: ' + computerScore;
+    }
+}
